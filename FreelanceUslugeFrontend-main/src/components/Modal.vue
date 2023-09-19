65f750df0ecfee6c3dbb5d2cf7c4fd8f29517b4a
AntonelaGalicc <85828969+AntonelaGalicc@users.noreply.github.com>
2023-09-19 14:32:26 +0200

    Add files via upload

---
+++ FreelanceUslugeFrontend-main/src/components/Modal.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="backdrop" @click.self="zatvoriModal">
+    <div class="modall">
+        <h3>Prijavi se</h3>
+        <form @submit.prevent="submitForm" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" id="email" v-model="formData.email" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password:</label>
+                <div class="d-flex">
+                <input v-bind:type="passwordFieldType" id="password" v-model="formData.password" class="form-control" required>
+                <button class="btn btn-link" @click="togglePasswordVisibility">
+                  <i v-bind:class="passwordFieldType === 'password' ? 'far fa-eye' : 'far fa-eye-slash'"></i>
+                </button>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-dark mb-3">Prijavi se</button>
+        </form>
+        <div class="d-flex justify-content-between">
+            <p>Nemate račun?</p>
+            <a href="#" class="text-decoration-none text-dark">Zaboravljena lozinka?</a>
+        </div>
+        <div class="text-start">
+            <router-link to="/register" class="p-1 text-decoration-none text-dark border border-dark rounded">Registriraj se</router-link>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            formData: {
+                email: '',
+                password: '',
+            },
+            passwordFieldType: 'password',
+        }
+    },
+    methods: {
+        zatvoriModal() {
+            this.$emit('zatvori');
+        },
+        submitForm() {
+            this.$emit('potvrdi', { email: this.formData.email, password: this.formData.password });
+        },
+        togglePasswordVisibility() {
+            this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+        },
+    }
+}
+</script>
+
+<style>
+    .modall{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+        z-index: 99999;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.7);
+        width: 100%;
+        height: 100%;
+        z-index: 99998;
+    }
+</style>
